a1ecc5293e16f8bf1654840a68ff84f13b7d28e0
tozetto <marcos@sisand.com.br>
2018-09-14 15:02:44 -0300

    Primeiro commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace NewRedimensionadorImagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRedimensionar = new System.Windows.Forms.Button();
+            this.lblPastaOrigem = new System.Windows.Forms.Label();
+            this.lblPastaDestino = new System.Windows.Forms.Label();
+            this.txtPastaOrigem = new System.Windows.Forms.TextBox();
+            this.txtPastaDestino = new System.Windows.Forms.TextBox();
+            this.ckbSubstituirArquivos = new System.Windows.Forms.CheckBox();
+            this.ckbIncluirSubPasta = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnRedimensionar
+            // 
+            this.btnRedimensionar.Location = new System.Drawing.Point(16, 155);
+            this.btnRedimensionar.Name = "btnRedimensionar";
+            this.btnRedimensionar.Size = new System.Drawing.Size(589, 39);
+            this.btnRedimensionar.TabIndex = 0;
+            this.btnRedimensionar.Text = "Redimensionar";
+            this.btnRedimensionar.UseVisualStyleBackColor = true;
+            this.btnRedimensionar.Click += new System.EventHandler(this.btnRedimensionar_Click);
+            // 
+            // lblPastaOrigem
+            // 
+            this.lblPastaOrigem.AutoSize = true;
+            this.lblPastaOrigem.Location = new System.Drawing.Point(13, 13);
+            this.lblPastaOrigem.Name = "lblPastaOrigem";
+            this.lblPastaOrigem.Size = new System.Drawing.Size(70, 13);
+            this.lblPastaOrigem.TabIndex = 1;
+            this.lblPastaOrigem.Text = "Pasta Origem";
+            // 
+            // lblPastaDestino
+            // 
+            this.lblPastaDestino.AutoSize = true;
+            this.lblPastaDestino.Location = new System.Drawing.Point(13, 93);
+            this.lblPastaDestino.Name = "lblPastaDestino";
+            this.lblPastaDestino.Size = new System.Drawing.Size(73, 13);
+            this.lblPastaDestino.TabIndex = 2;
+            this.lblPastaDestino.Text = "Pasta Destino";
+            // 
+            // txtPastaOrigem
+            // 
+            this.txtPastaOrigem.Location = new System.Drawing.Point(16, 30);
+            this.txtPastaOrigem.Name = "txtPastaOrigem";
+            this.txtPastaOrigem.Size = new System.Drawing.Size(589, 20);
+            this.txtPastaOrigem.TabIndex = 3;
+            // 
+            // txtPastaDestino
+            // 
+            this.txtPastaDestino.Location = new System.Drawing.Point(16, 110);
+            this.txtPastaDestino.Name = "txtPastaDestino";
+            this.txtPastaDestino.Size = new System.Drawing.Size(589, 20);
+            this.txtPastaDestino.TabIndex = 4;
+            // 
+            // ckbSubstituirArquivos
+            // 
+            this.ckbSubstituirArquivos.AutoSize = true;
+            this.ckbSubstituirArquivos.Location = new System.Drawing.Point(16, 56);
+            this.ckbSubstituirArquivos.Name = "ckbSubstituirArquivos";
+            this.ckbSubstituirArquivos.Size = new System.Drawing.Size(113, 17);
+            this.ckbSubstituirArquivos.TabIndex = 5;
+            this.ckbSubstituirArquivos.Text = "Substituir Arquivos";
+            this.ckbSubstituirArquivos.UseVisualStyleBackColor = true;
+            this.ckbSubstituirArquivos.CheckedChanged += new System.EventHandler(this.ckbSubstituirArquivos_CheckedChanged);
+            // 
+            // ckbIncluirSubPasta
+            // 
+            this.ckbIncluirSubPasta.AutoSize = true;
+            this.ckbIncluirSubPasta.Location = new System.Drawing.Point(135, 56);
+            this.ckbIncluirSubPasta.Name = "ckbIncluirSubPasta";
+            this.ckbIncluirSubPasta.Size = new System.Drawing.Size(111, 17);
+            this.ckbIncluirSubPasta.TabIndex = 6;
+            this.ckbIncluirSubPasta.Text = "Incluir Sub Pastas";
+            this.ckbIncluirSubPasta.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 211);
+            this.Controls.Add(this.ckbIncluirSubPasta);
+            this.Controls.Add(this.ckbSubstituirArquivos);
+            this.Controls.Add(this.txtPastaDestino);
+            this.Controls.Add(this.txtPastaOrigem);
+            this.Controls.Add(this.lblPastaDestino);
+            this.Controls.Add(this.lblPastaOrigem);
+            this.Controls.Add(this.btnRedimensionar);
+            this.Name = "Form1";
+            this.Text = "New Redimensionador de Imagens";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRedimensionar;
+        private System.Windows.Forms.Label lblPastaOrigem;
+        private System.Windows.Forms.Label lblPastaDestino;
+        private System.Windows.Forms.TextBox txtPastaOrigem;
+        private System.Windows.Forms.TextBox txtPastaDestino;
+        private System.Windows.Forms.CheckBox ckbSubstituirArquivos;
+        private System.Windows.Forms.CheckBox ckbIncluirSubPasta;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace NewRedimensionadorImagem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRedimensionar_Click(object sender, EventArgs e)
+        {
+            if ((!(ckbSubstituirArquivos.Checked)) && (txtPastaDestino.Text == "")) {
+                MessageBox.Show("Se não for substituir informe a pasta destino");
+            }
+            Image img;
+            string pasta = txtPastaOrigem.Text;
+            //string pastaNova = @"D:\Projetos\NewRedimensionadorImagem\Convertido";
+            string pastaNova = txtPastaDestino.Text;
+            if (!(ckbSubstituirArquivos.Checked) && (!(Directory.Exists(pastaNova))))
+                Directory.CreateDirectory(pastaNova);
+            if (Directory.Exists(pasta))
+            {
+                string[] fileEntries;                
+                if (ckbIncluirSubPasta.Checked)
+                    fileEntries = Directory.GetFiles(pasta, "*.*", System.IO.SearchOption.AllDirectories);
+                else
+                    fileEntries = Directory.GetFiles(pasta);
+                foreach (string fileName in fileEntries)
+                {
+                    FileInfo fileInfo = new FileInfo(fileName);
+                    string tempFile = Path.GetTempFileName();
+                    File.Copy(fileName, tempFile, true);
+                    img = Image.FromFile(tempFile);
+                    if (ckbSubstituirArquivos.Checked)
+                        SalvarImagemRedimensionada(img, fileName);
+                    else
+                        SalvarImagemRedimensionada(img, pastaNova + @"\" + Path.GetFileName(fileName));
+                    img.Dispose();
+                    File.Delete(tempFile);
+                }
+            }
+            MessageBox.Show("Feito!");
+        }
+
+        public static void SalvarImagemRedimensionada(Image img, string path)
+        {            
+            Image imgAux;
+            int percentSize = 0;
+            if ((img.Size.Width > 600) || (img.Size.Height > 600))
+            {
+                if (img.Size.Width > img.Size.Height) //paisagem            
+                    percentSize = Convert.ToInt32((600 * 100) / img.Size.Width);
+                else
+                    percentSize = Convert.ToInt32((600 * 100) / img.Size.Height);
+                imgAux = ScaleByPercent(img, percentSize); //tamanho em %
+                imgAux.Save(path);
+            }
+            else
+            {
+                img.Save(path);
+                imgAux = img;
+            }
+            FileInfo fileInfo = new FileInfo(path);
+            long fileLen = fileInfo.Length;
+            int percent = 100;
+            if (fileLen >= 500000)
+                percent = 70;
+            if ((fileLen < 500000) && (fileLen >= 300000))
+                percent = 75;
+            if ((fileLen < 300000) && (fileLen >= 100000))
+                percent = 80;
+            if (fileLen < 100000)
+                percent = 85;
+            SaveJpeg(path, imgAux, percent);
+            imgAux.Dispose();
+        }
+
+        public static void SaveJpeg(string path, Image img, int quality)
+        {
+            // Encoder parameter for image quality 
+            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            // JPEG image codec 
+            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;            
+            img.Save(path, jpegCodec, encoderParams);
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+
+            return null;
+        }
+
+        static Image ScaleByPercent(Image imgPhoto, int Percent)
+        {
+            float nPercent = ((float)Percent / 100);
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(destWidth, destHeight,
+                                     PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution,
+                                    imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            var attributes = new ImageAttributes();
+            attributes.SetWrapMode(WrapMode.TileFlipXY);
+
+            var destination = new Rectangle(0, 0, destWidth, destHeight);
+            grPhoto.DrawImage(imgPhoto, destination, 0, 0, sourceWidth, sourceHeight, GraphicsUnit.Pixel, attributes);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+        private void ckbSubstituirArquivos_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPastaDestino.Enabled = !(ckbSubstituirArquivos.Checked);
+        }
+
+
+        /*NOVAS FUNCOES*/
+
+        private void SaveTemporary(Bitmap bmp, MemoryStream ms, int quality, string sourcePath)
+        {
+            EncoderParameter qualityParam = new EncoderParameter
+                (System.Drawing.Imaging.Encoder.Quality, quality);
+            var codec = GetImageCodecInfo(sourcePath);            
+            var encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            bmp.Save(ms, codec, encoderParams);
+        }
+
+        private void SaveTemporary(Bitmap bmp, MemoryStream ms, int quality)
+        {
+            EncoderParameter qualityParam = new EncoderParameter
+                (System.Drawing.Imaging.Encoder.Quality, quality);            
+            ImageCodecInfo codec = GetEncoderInfo("image/jpeg");
+            var encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            bmp.Save(ms, codec, encoderParams);
+        }
+
+
+        public Bitmap ScaleImage(Bitmap image, double scale)
+        {
+            int newWidth = (int)(image.Width * scale);
+            int newHeight = (int)(image.Height * scale);
+
+            Bitmap result = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
+            result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                g.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+            return result;
+        }
+
+        public void ScaleImage(string sourcePath, string destinationPath, int allowedFileSizeInByte, short quality)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(sourcePath, FileMode.Open))
+                {
+                    Bitmap bmp = (Bitmap)Image.FromStream(fs);
+                    SaveTemporary(bmp, ms, quality, sourcePath);
+
+                    while (ms.Length > allowedFileSizeInByte)
+                    {
+                        double scale = Math.Sqrt((double)allowedFileSizeInByte / (double)ms.Length);
+                        ms.SetLength(0);
+                        bmp = ScaleImage(bmp, scale);
+                        SaveTemporary(bmp, ms, quality, sourcePath);
+                    }
+                    if (bmp != null)
+                        bmp.Dispose();
+                    if (sourcePath == destinationPath)
+                    {
+                        fs.Dispose();
+                        File.Delete(sourcePath);
+                    }
+                    SaveImageToFile(ms, destinationPath);
+                }
+            }
+        }
+
+        public void ScaleImage(Image image, string destinationPath, int allowedFileSizeInByte, short quality)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                
+                    Bitmap bmp = new Bitmap(image);
+                    SaveTemporary(bmp, ms, quality);
+
+                    while (ms.Length > allowedFileSizeInByte)
+                    {
+                        double scale = Math.Sqrt((double)allowedFileSizeInByte / (double)ms.Length);
+                        ms.SetLength(0);
+                        bmp = ScaleImage(bmp, scale);
+                        SaveTemporary(bmp, ms, quality);
+                    }
+
+                    if (bmp != null)
+                        bmp.Dispose();                    
+
+                    SaveImageToFile(ms, destinationPath);
+                
+            }
+        }
+
+
+        private void SaveImageToFile(MemoryStream ms, string destinationPath)
+        {
+            byte[] data = ms.ToArray();
+
+            using (FileStream fs = new FileStream(destinationPath, FileMode.Create))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+        }
+
+
+        private ImageCodecInfo GetImageCodecInfo(string sourcePath)
+        {
+            FileInfo fi = new FileInfo(sourcePath);
+
+            switch (fi.Extension)
+            {
+                case ".bmp": return ImageCodecInfo.GetImageEncoders()[0];
+                case ".jpg":
+                case ".jpeg": return ImageCodecInfo.GetImageEncoders()[1];
+                case ".gif": return ImageCodecInfo.GetImageEncoders()[2];
+                case ".tiff": return ImageCodecInfo.GetImageEncoders()[3];
+                case ".png": return ImageCodecInfo.GetImageEncoders()[4];
+                default: return null;
+            }
+        }
+        
+    }
+}
